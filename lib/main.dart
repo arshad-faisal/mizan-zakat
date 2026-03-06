68e591c0b14a70b96e15d8faed40e2031cc29e36
arshad-faisal <arshadfaisal786@gmail.com>
2026-03-06 14:46:45 +0530

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'screens/tribute_screen.dart';
+import 'screens/zakat_calculator_screen.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const ZakatApp());
+  runApp(const MizanZakatApp());
 }
 
-class ZakatApp extends StatelessWidget {
-  const ZakatApp({super.key});
+class MizanZakatApp extends StatelessWidget {
+  const MizanZakatApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Mizan Zakat',
       debugShowCheckedModeBanner: false,
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('en', ''),
-        Locale('ar', ''),
-        Locale('ur', ''),
-        Locale('hi', ''),
-      ],
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF1B5E20),
-          brightness: Brightness.light,
-        ),
+            seedColor: const Color(0xFF1B5E20)),
         useMaterial3: true,
+        fontFamily: 'Roboto',
       ),
-      home: const TributeScreen(),
+      home: const ZakatCalculatorScreen(),
     );
   }
 }
