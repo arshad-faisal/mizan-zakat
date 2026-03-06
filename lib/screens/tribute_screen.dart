128397fb3a81d3cd6f5920835ec6a76f3502c12d
arshad-faisal <arshadfaisal786@gmail.com>
2026-03-06 16:52:59 +0530

    tribute_screen.dart

--- lib/screens/tribute_screen.dart
+++ lib/screens/tribute_screen.dart
@@ -12,7 +12,7 @@ class _TributeScreenState extends State<TributeScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 5), () {
+    Future.delayed(const Duration(seconds: 9), () {
       if (mounted) {
         Navigator.of(context).pushReplacement(
           MaterialPageRoute(builder: (_) => const ZakatCalculatorScreen()),
